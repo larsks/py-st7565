265dab46904e43aa8d501dcc318b40c1e1876f8d
Lars Kellogg-Stedman <lars@redhat.com>
2015-07-07 16:31:40 -0400

    need find_packages() to find nested packaegs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     install_requires=open('requirements.txt').readlines(),
     version='0.1.0',
     name='st7565',
-    packages=['st7565'],
+    packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': [
             'stleds = st7565.cmd.stleds:main',
