8ec34b747e5fdab8ad47503f397ae9be8b4f3494
Lars Kellogg-Stedman <lars@redhat.com>
2015-07-06 23:20:16 -0400

    added a simple demos cript

---
+++ lcd_demo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import logging
+import time
+
+import st7565.backlight
+import st7565.lcd
+
+l = st7565.lcd.LCD(adafruit=True, flipped=True)
+b = st7565.backlight.Backlight()
+
+l.pos(3)
+l.puts('Hello')
+l.pos(4)
+l.puts('world!')
+
+for i in range(3):
+    for j in range(100):
+        b.backlight(j/100.0, 0, 0)
+        time.sleep(0.01)
+    for j in range(100):
+        b.backlight(1-(j/100.0), 0, 0)
+        time.sleep(0.01)
+
+b.all_leds_on()
